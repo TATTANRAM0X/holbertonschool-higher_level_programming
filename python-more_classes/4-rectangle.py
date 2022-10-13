c29a130b6908c90747e97dda28181ade037d9d2d
TATTARAM0X <jhonatan.ramox@gmail.com>
2022-10-12 19:43:28 -0700

    Write a class Rectangle that defines a rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = velue
+        self.__height = value
 
     """ setter and getter of width property """
     @property
