2ea50e24cd1528fd434fef6c351b968b8a6be9d9
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-13 23:10:14 -0500

    All task

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Module to create an empty class"""
+""" BaseGeometry class """
 
 
-class BaseGeometry():
-    """An class named BaseGeometry
-    Attributes:
-    attr1(area): Raises an exception
-    """
-
+class BaseGeometry:
+    """ Public instance method that raises an exception """
     def area(self):
-        """raises an exception"""
         raise Exception("area() is not implemented")
 
+    """
+    Public instance method that validates if value is
+    an integer and greater than zero
+    """
     def integer_validator(self, name, value):
-        """validates input"""
-        if not isinstance(value, int):
-            raise TypeError(f"{value} must be an integer")
-
+        self.name = name
+        self.value = value
+        if type(value) != int:
+            raise TypeError(f"{self.name} must be an integer")
         if value <= 0:
-            raise ValueError(f"{value} must be greater than 0")
+            raise ValueError(f"{self.name} must be greater than 0")
