a931e1ef83fa2fb9d42ac71dc5576c68dc810df3
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-20 15:51:09 -0500

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,10 @@
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        ''' Inicialezes an instance '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
