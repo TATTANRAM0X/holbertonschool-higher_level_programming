7596c95f5e0173d7bd354d6c4efb7d91b8173110
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 15:28:13 -0500

    And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,8 +3,13 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     ''' Square class is created instruction of the task '''
     def __init__(self, size, x=0, y=0, id=None):
         ''' Inicialzes instance of Square class '''
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns string representation of Square class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
