9f18ddd68bc81d3d770ed8a6e1f0f6fba23ca1db
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 14:11:36 -0500

    __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,6 @@ class Rectangle(Base):
             for column in range(self.__width):
                 print("#", end="")
             print()
+    def __str__(self):
+        ''' Returns a string representation of a Rectangle '''
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
