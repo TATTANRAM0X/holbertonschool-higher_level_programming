793d630fdf87deb368f2d33a050867dde07db620
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 16:27:03 -0500

    Square instance to dictionary representation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -48,3 +48,6 @@ class Square(Rectangle):
                 self.y = kwargs["y"]
 
     def to_dictionary(self):
+        """Return format of dictionary"""
+        dictionary = {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
+        return dictionary
