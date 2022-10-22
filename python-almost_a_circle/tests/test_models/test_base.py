a501ebaa7f44a1ea5134db125322654d09168ad0
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 22:03:33 -0500

    Update tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-''' Tests a base_models '''
-
-
+"""Unittest for Base class
+"""
 import unittest
 from models.base import Base
-
-class TestBase(unittest.TestCase):
-    def test_id_base(self):
-
-        b1 = Base()
-        self.assertAlmostEqual(b1.id, 1)
-
-        b2 = Base()
-        self.assertAlmostEqual(b2.id, 2)
-
-        b3 = Base()
-        self.assertAlmostEqual(b3.id, 3)
-
-        b4 = Base(12)
-        self.assertAlmostEqual(b4.id, 12)
-
-        b5 = Base()
-        self.assertAlmostEqual(b5.id, 4)
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-if  __name__  ==  '__main__':
-    unittest.main()
+class TestBaseClass(unittest.TestCase):
+    """
+    Unit test class to check Base class
+    """
+    pass
