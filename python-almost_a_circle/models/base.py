654062a7a4556f3194d2102e158c667b9070be8e
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 20:32:23 -0500

    File to instances

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-''' File for class Base '''
+'''Write the class Base'''
+
+
 import json
 
 
 class Base:
-    ''' Base class task instruction '''
+    '''Initialize the class'''
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        ''' Inicialezes an instance '''
+        '''Initialize'''
         if id is not None:
             self.id = id
         else:
@@ -17,44 +20,46 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        ''' Returns the JSON string representation of a list of dict '''
-        if list_dictionaries is None:
-            list_dictionaries = []
-        dictJson = json.dumps(list_dictionaries)
-        return dictJson
+        '''returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file'''
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                l_dic = [x.to_dictionary() for x in list_objs]
+                file.write(Base.to_json_string(l_dic))
 
     @staticmethod
     def from_json_string(json_string):
-        ''' Returns the list of the JSON string representation json_string '''
+        '''returns the list of the JSON string representation json_string'''
         if json_string is None or len(json_string) == 0:
             return []
-        strJson = json.loads(json_string)
-        return strJson
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        ''' Returns an instance with all attributes already set '''
-
-        from models.rectangle import Rectangle
-        from models.square import Square
-
+        '''returns an instance with all attributes already set'''
         if cls.__name__ == "Rectangle":
-            dummyInstance = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummyInstance = cls(1)
-        dummyInstance.update(**dictionary)
-        return dummyInstance
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return(dummy)
 
     @classmethod
     def load_from_file(cls):
-        '''  Returns a list of instances '''
+        '''returns a list of instances'''
         filename = cls.__name__ + ".json"
-        list = []
         try:
-            with open(filename, 'r') as f:
-                list = cls.from_json_string(f.read())
-            for i, e in enumerate(list):
-                list[i] = cls.create(**list[i])
-        except:
-            pass
-        return list
+            with open(filename, "r", encoding="utf-8") as file:
+                l_dic = Base.from_json_string(file.read())
+                return [cls.create(**dicti) for dicti in l_dic]
+        except Exception:
+            return []
