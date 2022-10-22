09e4a9ce62b97aa59c7ca5396804a2f5da812113
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-10-21 19:36:31 -0500

    tests

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,6 +34,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         ''' Returns an instance with all attributes already set '''
+
+        from models.rectangle import Rectangle
+        from models.square import Square
+
         if cls.__name__ == "Rectangle":
             dummyInstance = cls(1, 1)
         elif cls.__name__ == "Square":
@@ -43,51 +47,14 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        '''  Returns a list of instances '''
         filename = cls.__name__ + ".json"
-        l = []
+        list = []
         try:
             with open(filename, 'r') as f:
-                l = cls.from_json_string(f.read())
-            for i, e in enumerate(l):
-                l[i] = cls.create(**l[i])
+                list = cls.from_json_string(f.read())
+            for i, e in enumerate(list):
+                list[i] = cls.create(**list[i])
         except:
             pass
-        return l
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """serializes a list of Rectangles/Squares in csv"""
-        filename = cls.__name__ + ".csv"
-        with open(filename, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            if cls.__name__ is "Rectangle":
-                for obj in list_objs:
-                    csv_writer.writerow([obj.id, obj.width, obj.height,
-                                         obj.x, obj.y])
-            elif cls.__name__ is "Square":
-                for obj in list_objs:
-                    csv_writer.writerow([obj.id, obj.size, obj.x, obj.y])
-
-    @classmethod
-    def load_from_file_csv(cls):
-        """deserializes a list of Rectangles/Squares in csv"""
-        filename = cls.__name__ + ".csv"
-        l = []
-        try:
-            with open(filename, 'r') as csvfile:
-                csv_reader = csv.reader(csvfile)
-                for args in csv_reader:
-                    if cls.__name__ is "Rectangle":
-                        dictionary = {"id": int(args[0]),
-                                      "width": int(args[1]),
-                                      "height": int(args[2]),
-                                      "x": int(args[3]),
-                                      "y": int(args[4])}
-                    elif cls.__name__ is "Square":
-                        dictionary = {"id": int(args[0]), "size": int(args[1]),
-                                      "x": int(args[2]), "y": int(args[3])}
-                    obj = cls.create(**dictionary)
-                    l.append(obj)
-        except:
-            pass
-        return l
+        return list
