5db1d9edee7124af0bcd77d9b3e3384b88e72944
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-11-18 11:26:33 -0500

    SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Script that list all states from database
+'''
+from sys import argv
+import MySQLdb
+
+
+def filter_states():
+    '''
+    This function lists all states from the database hbtn_0e_0_usa
+    sorted in ascending order
+    '''
+    data_base = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3])
+
+    cur = data_base.cursor()
+    query = "SELECT id, name FROM states WHERE BINARY name = '%s' \
+             ORDER BY states.id ASC"%(argv[4])
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    data_base.close
+
+
+if __name__ == "__main__":
+    filter_states()
