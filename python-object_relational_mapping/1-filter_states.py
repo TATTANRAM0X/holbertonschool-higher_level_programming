6e915a1101df95c795f28b63734b8cbd01649504
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-11-17 23:02:24 -0500

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Script that list all states from database
+'''
+from sys import argv
+import MySQLdb
+
+
+def states_with_N():
+    '''
+    This function lists all states from the database hbtn_0e_0_usa
+    sorted in ascending order
+    '''
+    data_base = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3])
+
+    cur = data_base.cursor()
+    cur.execute('SELECT id, name FROM states WHERE name LIKE BINARY 'N%' \
+             ORDER BY states.id ASC')
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    data_base.close
+
+
+if __name__ == "__main__":
+    states_with_N()
