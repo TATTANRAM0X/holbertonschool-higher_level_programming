00ac322b956cc21538540129e3eae7f574091eaf
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-19 15:58:55 -0500

    update

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const argument1 = process.argv[2];
-const argument2 = process.argv[3];
+let argument1 = process.argv[2];
+let argument2 = process.argv[3];
 console.log(argument1 + ' is ' + argument2);
