a3cf08e04eda175d1e801f1028541c8d6f513fbd
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-19 15:36:00 -0500

    update

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length < 3)
-{
+if (process.argv.length < 3) {
   console.log('No argument');
 }
 
-if (process.argv.length === 3)
-{
+if (process.argv.length === 3) {
   console.log('Argument found');
 }
 
-if (process.argv.length > 3)
-{
+if (process.argv.length > 3) {
   console.log('Arguments found');
 }
