ec5d7d9fdc796fe992fd33d3516bdb3bed6179d7
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-20 16:09:30 -0500

    Task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (args.length < 4) {
+  console.log(0);
+} else {
+  args.sort(comparison)
+
+  num = (args[args.length - 2])
+  console.log(num);
+}
+
+function comparison (a, b) {
+  return a - b;
+};
