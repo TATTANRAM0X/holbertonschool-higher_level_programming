3c02f9cf1d94d6c1a41fb8fdcc640795741ed8a0
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-21 17:59:30 -0500

    update

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
-  for (const position in list) {
-    if (list[position] === searchElement) {
+  for (const position of list) {
+    if (position === searchElement) {
       occurences++;
     }
   }
