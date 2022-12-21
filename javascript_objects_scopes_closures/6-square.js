ba9dbc276478afa8de0fc23754a03f4e2ffeca1c
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-21 17:08:32 -0500

    update

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const Sq = require('./5-square.js');
 
 module.exports = class Square extends Sq {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
