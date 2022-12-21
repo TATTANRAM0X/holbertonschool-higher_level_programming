0328c2edcdfb0ad978e8f19095815c36cc9afba3
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-20 22:07:13 -0500

    update

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -6,8 +6,8 @@ const myObject = {
 console.log(myObject);
 
 myObject.incr = function () {
-  this.value = (this.value + 1)
-}
+  this.value = (this.value + 1);
+};
 
 myObject.incr();
 console.log(myObject);
@@ -15,4 +15,3 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
