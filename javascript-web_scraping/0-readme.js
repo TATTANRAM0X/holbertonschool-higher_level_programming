ba191235a97c29b34f268e8217c30964cf106205
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-22 17:44:57 -0500

    Update

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const file = require('fs');
 
 file.readFile(process.argv[2], 'utf-8', (error, data) => {
-    if (error) {
-        throw error;
-    }
-    console.log(data)
-})
+  if (error) {
+    throw error;
+  }
+  console.log(data);
+});
