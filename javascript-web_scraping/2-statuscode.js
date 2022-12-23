820a30b65418cf7d0af4b7d74afd41fd45d565c3
TATTANRAM0X <jhonatan.ramox@gmail.com>
2022-12-22 19:03:20 -0500

    task

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else console.log(response.statusCode);
+request(url, function (error, response) {
+  if (!error) console.log(`code: ${response.statusCode}`);
 });
